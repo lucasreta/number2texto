e2fc01725058e09fa7f10506f6ded0a735d2acee
Lucas Reta <1.lucasreta@gmail.com>
2020-06-10 00:38:32 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+/**
+ * Converts Number to Text.
+ *
+ * @module numberToText
+ */
+module.exports = require('./lib/numberToText');
---
+++ test/index.js
@@ -0,0 +1,8 @@
+
+const n2t = require('../index.js');
+
+console.log(n2t(203,'es'));
+console.log(n2t(1203,'es'));
+console.log(n2t(435325,'es'));
+// console.log(n2t(435325,'en',__dirname + '/languages/'));
+// console.log(n2t(435325,'en'));
