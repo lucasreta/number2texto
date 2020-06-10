011ddf4481c58bd1b21044ba9fde784694a189b9
Lucas Reta <1.lucasreta@gmail.com>
2020-06-10 02:44:11 -0300

    adds primitive english support, groundwork for decimal support, improves bugs

--- test/index.js
+++ test/index.js
@@ -1,8 +1,27 @@
 
 const n2t = require('../index.js');
 
-console.log(n2t(203,'es'));
-console.log(n2t(1203,'es'));
-console.log(n2t(435325,'es'));
+console.log(n2t(14, 'es'));
+console.log(n2t(20, 'es'));
+console.log(n2t(21, 'es'));
+console.log(n2t(100, 'es'));
+console.log(n2t(101, 'es'));
+console.log(n2t(10, 'en'));
+console.log(n2t(18, 'en'));
+console.log(n2t(20, 'en'));
+console.log(n2t(178, 'en'));
+// console.log(n2t(203, 'es'));
+// console.log(n2t(1203, 'es'));
+// console.log(n2t(435325, 'es'));
+// console.log(n2t(1000000, 'es'));
+// console.log(n2t(1000001, 'es'));
+// console.log(n2t(1000015, 'es'));
+// console.log(n2t(1000016, 'es'));
+// console.log(n2t(1000317, 'es'));
+// console.log(n2t(1001018, 'es'));
+// console.log(n2t(1040019, 'es'));
+// console.log(n2t(1500089, 'es'));
+// console.log(n2t(1500549, 'es'));
+// console.log(n2t(1540529, 'es'));
 // console.log(n2t(435325,'en',__dirname + '/languages/'));
 // console.log(n2t(435325,'en'));
